7717e80aa453f21c1e632e70c71656c6aebc94e8
Antonio Ruberto <anto.ruberto@gmail.com>
2016-01-25 02:53:38 -0500

    button

--- demo/index.js
+++ demo/index.js
@@ -7,7 +7,41 @@ class Demo extends Component {
   render() {
     return (
       <div>
-        <Button/>
+        <Button>Default Button</Button>
+        <br/>
+        <Button className='hello-world'>Button With Class</Button>
+        <br/>
+        <Button fSize='tiny'>Tiny Button</Button>
+        <br/>
+        <Button fSize='small'>Small Button</Button>
+        <br/>
+        <Button fSize='large'>Large Button</Button>
+        <br/>
+        <Button expanded>Expanded Button</Button>
+        <br/>
+        <Button expanded fSize='tiny'>Tiny Expanded Button</Button>
+        <br/>
+        <Button fStyle='secondary'>Secondary Button</Button>
+        <br/>
+        <Button fStyle='success'>Success Button</Button>
+        <br/>
+        <Button fStyle='alert'>Alert Button</Button>
+        <br/>
+        <Button fStyle='warning'>Warning Button</Button>
+        <br/>
+        <Button disabled>Disabled Button</Button>
+        <br/>
+        <Button disabled fStyle='alert'>Disabled Alert Button</Button>
+        <br/>
+        <Button hollow>Hollow Button</Button>
+        <br/>
+        <Button fStyle='secondary' hollow>Hollow Secondary Button</Button>
+        <br/>
+        <Button fStyle='success' hollow>Hollow Success Button</Button>
+        <br/>
+        <Button fStyle='alert' hollow>Hollow Alert Button</Button>
+        <br/>
+        <Button fStyle='warning' hollow>Hollow Warning Button</Button>
       </div>
     );
   }
--- src/button/index.js
+++ src/button/index.js
@@ -1,11 +1,39 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
 
 import style from './style';
 
 export default class Button extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string,
+    disabled: PropTypes.bool,
+    expanded: PropTypes.bool,
+    fSize: PropTypes.oneOf(['tiny', 'small', 'large']),
+    fStyle: PropTypes.oneOf(['secondary', 'success', 'alert', 'warning']),
+    hollow: PropTypes.bool
+  };
+
   render() {
+    const {className, children, disabled, expanded, fSize, fStyle, hollow} = this.props;
+    const classes = {
+      [style.button]: true,
+      [style.disabled]: disabled,
+      [style.expanded]: expanded,
+      [style.tiny]: fSize === 'tiny',
+      [style.small]: fSize === 'small',
+      [style.large]: fSize === 'large',
+      [style.secondary]: fStyle === 'secondary',
+      [style.success]: fStyle === 'success',
+      [style.alert]: fStyle === 'alert',
+      [style.warning]: fStyle === 'warning',
+      [style.hollow]: hollow
+    };
+
     return (
-      <button className={style.button}>Hello World!</button>
+      <button className={classNames(className, classes)}>
+        {children}
+      </button>
     );
   }
 }
