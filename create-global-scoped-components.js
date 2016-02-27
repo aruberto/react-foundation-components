7eb157372e7df67ce7d5c955c26911f6960b67c1
Antonio Ruberto <aruberto@users.noreply.github.com>
2016-02-27 15:55:01 -0500

    global scoped component creation

---
+++ create-global-scoped-components.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console, prefer-arrow-callback, prefer-template */
+
+const path = require('path');
+const fs = require('fs-extra');
+const through2 = require('through2');
+const s = require('underscore.string');
+
+const filter = through2.obj(function componentDirectoryFilter(item, enc, next) {
+  if (item.stats.isFile()) {
+    const fileName = path.basename(item.path);
+
+    if (fileName === 'index.js') {
+      const directory = path.dirname(item.path);
+      const createPath = path.join(directory, 'create.js');
+      const stylesPath = path.join(directory, 'styles.js');
+      const cssPath = path.join(directory, '_styles.scss');
+
+      try {
+        fs.accessSync(item.path, fs.F_OK);
+        fs.accessSync(createPath, fs.F_OK);
+        fs.accessSync(stylesPath, fs.F_OK);
+        fs.accessSync(cssPath, fs.F_OK);
+
+        this.push(item);
+      } catch (e) {
+        // Not component directory
+      }
+    }
+  }
+
+  next();
+});
+
+fs.walk('.' + path.sep + 'lib')
+  .pipe(filter)
+  .on('data', function processDirectory(item) {
+    const globalPath =
+      s(item.path)
+        .reverse()
+        .replace(
+          s('lib' + path.sep).reverse().value(),
+          s('lib' + path.sep + 'global' + path.sep).reverse().value()
+        )
+        .reverse()
+        .value();
+    const relativePath =
+      path.relative(path.dirname(globalPath), path.dirname(item.path)).split(path.sep).join('/');
+
+    const stylesPath = '\'' + relativePath + '/' + 'styles\'';
+    const createPath = '\'' + relativePath + '/' + 'create\'';
+    const content =
+      s(fs.readFileSync(item.path, 'utf8'))
+        .replace('\'./_styles.scss\'', stylesPath)
+        .replace('\'./create\'', createPath)
+        .value();
+
+    fs.ensureDirSync(path.dirname(globalPath));
+    fs.writeFileSync(globalPath, content, 'utf8');
+  });
