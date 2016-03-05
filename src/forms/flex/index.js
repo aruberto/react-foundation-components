c4f5f2968a0f6f9cc2a26999ea0b86a583501a76
Antonio Ruberto <aruberto@users.noreply.github.com>
2016-03-05 14:24:42 -0500

    better grid mode

--- src/forms/flex/index.js
+++ src/forms/flex/index.js
@@ -8,16 +8,10 @@ const {
   FormFieldInput,
   FormFieldLabel,
   FormFieldError,
-  FormFieldGroup,
-  FormFieldRight,
-  FormFieldColumn,
 } = create(styles, TextAlignment, Row, Column);
 FormField.Input = FormFieldInput;
 FormField.Label = FormFieldLabel;
 FormField.Error = FormFieldError;
-FormField.Group = FormFieldGroup;
-FormField.Right = FormFieldRight;
-FormField.Column = FormFieldColumn;
 
 export default FormField;
 export {
@@ -25,7 +19,4 @@ export {
   FormFieldInput,
   FormFieldLabel,
   FormFieldError,
-  FormFieldGroup,
-  FormFieldRight,
-  FormFieldColumn,
 };
